985557eb94a7f8071b37f18736612fe7f59ab528
parmida shoeibzade <parmida.shoeibzade4@Gmail.com>
2024-12-05 12:16:45 +0300

    animation added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Button = ({ title ,id ,rightIcon ,leftIcon ,containerClass}) => {
   return (
-    <Button id={id} className={`group relative z-10 w-fit cursor-pointer 
-      px-7 rounded-full overflow-hidden bg-violet-50 py-3 text-black ${containerClass}`}>{leftIcon}</Button>
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer 
+      px-7 rounded-full overflow-hidden py-3 text-black ${containerClass}`}>
+        {leftIcon}
+
+        <span className='relative text-xs incline-flex overflow-hidden font-general uppercase'>
+          <div>{title}</div>
+        </span>
+    </button>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
 import  { useRef, useState } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 
 
 const Hero = () => {
@@ -21,6 +24,49 @@ const Hero = () => {
   }
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1
+
+  useGSAP(() => {
+    if(hasClicked){
+      gsap.set('#next-video', {visibility:'visible'});
+
+      gsap.to('#next-video', {
+        transformOrigin : 'center center',
+        scale : 1, 
+        width : '100%',
+        height : '100%',
+        duration : 1 ,
+        ease : 'power1.inOut' ,
+        onStart : () => nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video' , {
+        transformOrigin: 'center center',
+        scale: 0 ,
+        duration : 1.5 ,
+        ease : 'power1.inOut'
+      })
+    }
+    
+  } , {dependencies: [currentIndex] , revertOnUpdate: true})
+
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius:'0 0 40% 10%'
+    });
+
+    gsap.from('#video-frame' , {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius:'0 0 0 0',
+      ease:'power1.inOut',
+      scrollTrigger :{
+        trigger: '#video-frame',
+        start : 'center center',
+        end : 'buttom center',
+        scrub: true,
+      }
+    })
+  })
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -45,14 +91,15 @@ const Hero = () => {
             <video
               ref={nextVideoRef}
               src={getVideoSrc(currentIndex)}
-              loopmuted
+              loop
+              muted
               id="next-video"
               className='absolute-center absolute z-20 invisible size-64 object-cover object-center'
               onLoadedData={handleVideoLoad}/>
 
             <video 
               src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex )}
-              // autoPlay
+              autoPlay
               loop
               muted
               className='absolute left-0 top-0 object-center size-full object-cover'
@@ -69,10 +116,14 @@ const Hero = () => {
               <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
                 Enter the Metagame Layer <br/> Unleash the play economy
               </p>
-              <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass="bg-yellow-300 flex-center gap-1"/>
+              <Button id="watch-trailer" title="watch trailer" leftIcon={<TiLocationArrow/>} containerClass="bg-yellow-300 flex-center gap-1"/>
             </div>
           </div>
         </div>
+
+        <h1 className='special-font hero-heading absolute bottom-5 right-5  text-black'>
+          G<b>A</b>ming
+        </h1>
     </div>
   )
 }
